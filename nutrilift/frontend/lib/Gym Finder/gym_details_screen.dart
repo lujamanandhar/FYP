87eab89aae40c3cd4b6696b2ff1a63b300f28f1c
Luja Ratna Manandhar <lujamanandhar13@gmail.com>
2026-01-28 23:06:35 +0545

    error fixing in gym detsils screen

--- nutrilift/frontend/lib/Gym Finder/gym_details_screen.dart
+++ nutrilift/frontend/lib/Gym Finder/gym_details_screen.dart
@@ -218,7 +218,7 @@ class _GymDetailsScreenState extends State<GymDetailsScreen>
             ),
           ),
 
-          // Tab Content
+          
           SliverFillRemaining(
             child: TabBarView(
               controller: _tabController,
