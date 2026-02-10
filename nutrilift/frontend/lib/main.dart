a7c9205a871911d39066fade7618364059570d72
Luja Ratna Manandhar <lujamanandhar13@gmail.com>
2026-02-10 23:12:03 +0545

    updated main page

--- nutrilift/frontend/lib/main.dart
+++ nutrilift/frontend/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Assign navigator key to enable access from ErrorHandler service here
+      // Assign navigator key to enable access from ErrorHandler service 
       navigatorKey: navigatorKey,
       title: 'NutriLift',
       theme: ThemeData(
